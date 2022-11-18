4b0c78a3521313d1a4f97f4fbd79b44245278a84
Mikaela Grace <mikaelagrace@users.noreply.github.com>
2022-11-17 23:40:37 -0500

    Update sentiment_analysis.py
    No need to merge the Pull Request if you don't want to, it's just a way for me to suggest updates with inline diffs!

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -1,15 +1,21 @@
 from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk
-nltk.download(["names", "stopwords", "movie_reviews", "vader_lexicon", "punkt"])
+nltk.download(["names", "stopwords", "movie_reviews", "vader_lexicon", "punkt"]) # do you need this line?
+
+"""
+ It’s best practice to have your code in a descriptive method or small class, if possible, rather than running at the top-level.  makes it easier for other modules to import the functionality later if needed! 
+
+"""
+
+
 sia = SentimentIntensityAnalyzer()
 def sentiment_analysis():
     with open('sentiment_analysis/dataset/tiny_movie_reviews_dataset.txt') as f:
-        while True:
-            line = f.readline()
-            if not line:
-                break
+        lines = f.readlines()
+        for line in lines:
+            if not line: continue 
             sentiment = sia.polarity_scores(line)
             if (sentiment['neg'] > sentiment['pos']):
                 print('NEGATIVE')
-            elif (sentiment['neg'] < sentiment['pos']):
+            else: # dont need the extra conditional
                 print('POSITIVE')
